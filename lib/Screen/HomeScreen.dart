7156dc0441a80005f0d3c49c0f73a1a2d04e60bf
AnusRaza1807 <68642901+AnusRaza1807@users.noreply.github.com>
2024-09-30 10:01:09 +0500

    Update HomeScreen.dart

--- lib/Screen/HomeScreen.dart
+++ lib/Screen/HomeScreen.dart
@@ -1,8 +1,4 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:news_app/API/News_API.dart';
-import 'package:news_app/Model/NewsHeadLinesModel.dart';
 import 'package:news_app/Screen/CategoriesScreen.dart';
 import 'package:news_app/Screen/HeadLinseWidget.dart';
 
@@ -14,31 +10,27 @@ class Homescreen extends StatefulWidget {
 }
 
 class _HomescreenState extends State<Homescreen> {
-  NewsApi _newsApi = NewsApi();
-
   @override
   Widget build(BuildContext context) {
-    final height = MediaQuery.sizeOf(context).height * 1;
-    final width = MediaQuery.sizeOf(context).width * 1;
-
     return Scaffold(
         appBar: AppBar(
-          title: Text("News App"),
+          title: const Text("News App"),
           actions: [
             IconButton(
-              icon: Icon(Icons.more_vert_outlined),
+              icon: const Icon(Icons.more_vert_outlined),
               onPressed: () {
                 Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => Categoriesscreen()));
+                        builder: (context) => const Categoriesscreen()));
               },
             )
           ],
         ),
         body: ListView(
+          // ignore: prefer_const_literals_to_create_immutables
           children: [
-            Headlinsewidget(),
+            const Headlinsewidget(),
           ],
         ));
   }
